0c3327a5366fc7237a1b0407b49870e33fccaa37
Charles Grant Nichols <grant@inkdots.com>
2021-05-13 13:29:22 -0500

    functionallity done

--- shop-project/src/Containers/ComeBackOlder.js
+++ shop-project/src/Containers/ComeBackOlder.js
@@ -6,7 +6,8 @@ class ComeBackOlder extends React.Component {
         return (
             <div>
                 <img src="https://angelasalazarrrblog.files.wordpress.com/2014/04/i-chzbgr.jpg" alt="haha you need to grow up"></img>
-                <p>link to drinking rules website</p>
+                <br/>
+                <a target= "_blank" href="https://www.consumer.ftc.gov/articles/0388-alcohol-laws-state">CLICK ME</a>
             </div>)
     }
 }
--- shop-project/src/Containers/RequestForm.js
+++ shop-project/src/Containers/RequestForm.js
@@ -5,14 +5,16 @@ class RequestForm extends React.Component {
     state = {
         name: "",
         image: "",
-        description: ""
+        description: "",
+        type: "Other",
+        quantity: "1"
     }
 
 
     onSubmit = (e) => {
         e.preventDefault()
         this.props.requestItem(this.state)
-        
+
 
     }
 
@@ -34,6 +36,18 @@ class RequestForm extends React.Component {
         })
     }
 
+    onNewQuantity = event => {
+        this.setState({
+            quantity: event.target.value
+        })
+    }
+
+    onNewType = event => {
+        this.setState({
+            type: event.target.value
+        })
+    }
+
 
     render() {
         return (
@@ -46,6 +60,24 @@ class RequestForm extends React.Component {
                     <br />
                     <input type="text" name="description" placeholder="Enter a items description..." className="input-text" onChange={this.onNewDescription} value={this.state.description} />
                     <br />
+                    <select onChange={this.onNewQuantity} value={this.state.quantity}>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                    <select onChange={this.onNewType} value={this.state.type}> Type:
+                    <option value="Other">Other</option>
+                        <option value="Whiskey">Whiskey</option>
+                        <option value="Vodka">Vodka</option>
+                        <option value="Gin">Gin</option>
+                        <option value="Wine">Wine</option>
+                        <option value="Tequila">Tequila</option>
+                        <option value="Beer">Beer</option>
+                    </select>
+
+
                     <input type="submit" name="submit" value="Add Missing Item" className="submit" />
                 </form>
             </div>
